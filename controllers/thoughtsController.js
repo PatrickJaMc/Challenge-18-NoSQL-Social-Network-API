293bb37cac840606ace9bfbd7e1465caf78ff804
C12 <mcpartlandpatrick@gmail.com>
2023-08-07 13:32:35 -0400

    updated thoughts controller

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,94 @@
+const {User, Thought} = require('../models');
+
+module.exports = {
+  async getAllThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+
+  async getSingleThought(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.userId })
+        .select('-__v')
+        .populate('Thought');
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+
+  // create a new thought
+  async createThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.create(req.body);
+      res.json(dbThoughtData);
+      console.log(`Thought created`);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+
+  //Update a thought
+  async updateThought (req, res) {
+    try{
+      const thought = Thought.findOneAndUpdate({
+        _id: req.params.id
+      }, {
+        $set: req.body,
+        new: true
+      });
+      res.json(thought);
+      console.log(`Thought updated`);
+    } catch (err){
+        res.status(500).json(err);
+    }
+  },
+
+  //Delete a thought
+  async deleteThought (req, res) {
+    try{
+     const thought = Thought.findOneAndDelete({
+        _id: req.params.id
+     });
+     res.json(thought);
+     console.log(`Deleted thought`);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+  },
+
+  //Add friend
+  async addFriend(req, res) {
+    try{
+     const newFriend = User.findOneAndUpdate(
+        {_id: req.params.id },
+        { $addToSet:  {friends: req.params.friendsId}},
+        { new: true}
+        );
+     res.json(newFriend);
+     console.log(`New friend ${newFriend} added`);
+    } catch(err) {
+        res.status(500).json(err)
+    }
+  },
+
+  //Remove friend
+  async removeFriend(req, res) {
+    try{
+      removeFriend = User.findOneAndUpdate(
+      { _id: req.params.id},
+      { $pull: { friends: req.params.friendsId}}, 
+      {new: true}
+      )
+    } catch (err) {
+        res.status(500).json(err)
+    }
+  }
+};
